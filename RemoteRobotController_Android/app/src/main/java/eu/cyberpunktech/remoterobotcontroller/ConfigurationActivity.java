3d1298ad97bd2a7b637dea8f0a4cdbcea1b72a78
Viktor Vaňo <viktorvano@gmail.com>
2022-04-14 20:20:41 +0200

    fixed spaces bug in IP

--- RemoteRobotController_Android/app/src/main/java/eu/cyberpunktech/remoterobotcontroller/ConfigurationActivity.java
+++ RemoteRobotController_Android/app/src/main/java/eu/cyberpunktech/remoterobotcontroller/ConfigurationActivity.java
@@ -5,6 +5,8 @@ import static eu.cyberpunktech.remoterobotcontroller.Variables.*;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,7 +34,43 @@ public class ConfigurationActivity extends AppCompatActivity {
         buttonSave = findViewById(R.id.buttonBack);
 
         editTextSTM32Address.setText(stringSTM32IP);
+        editTextSTM32Address.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.toString().contains(" "))
+                    editTextSTM32Address.setText(charSequence.toString().replace(" ", ""));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
         editTextAndroidCamera.setText(String.valueOf(stringAndroidIP));
+        editTextAndroidCamera.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.toString().contains(" "))
+                    editTextAndroidCamera.setText(charSequence.toString().replace(" ", ""));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- RemoteRobotController_Android/app/src/main/java/eu/cyberpunktech/remoterobotcontroller/Variables.java
+++ RemoteRobotController_Android/app/src/main/java/eu/cyberpunktech/remoterobotcontroller/Variables.java
@@ -5,7 +5,7 @@ public class Variables {
     public static String stringAndroidIP = "example2.ddns.net";
     public static int stm32StatusUpdatePeriod = 1000;
 
-    public static final String version = "Version 20220227";
+    public static final String version = "Version 20220414";
     public static final int cameraPort = 7770;
 
     public static final String CONFIGURATION = "configuration";

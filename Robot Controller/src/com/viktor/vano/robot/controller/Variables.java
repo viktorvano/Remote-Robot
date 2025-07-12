6b4de1077cb4743a4f0728d1c664f7e61917f0be
Viktor Vaňo <viktorvano@gmail.com>
2025-07-12 15:43:37 +0200

    IP update

--- Robot Controller/src/com/viktor/vano/robot/controller/Variables.java
+++ Robot Controller/src/com/viktor/vano/robot/controller/Variables.java
@@ -1,8 +1,8 @@
 package com.viktor.vano.robot.controller;
 
 public class Variables {
-    public static String stringSTM32IP = "127.0.0.1";
-    public static String stringAndroidIP = "127.0.0.1";
+    public static String stringSTM32IP = "192.168.1.101";
+    public static String stringAndroidIP = "192.168.1.102";
     public static int stm32StatusUpdatePeriod = 1000;
     public static double distanceProgressRange = 200.0;
 }
